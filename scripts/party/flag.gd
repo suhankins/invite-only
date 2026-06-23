8821519d9ede1692d731e3fb52f19774b16a3fa7
Dimitri Sukhankin <berna@detta.dev>
2026-06-24 02:05:58 +0400

    added basic coding for flag pole minigame

---
+++ scripts/party/flag.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+@export var minimum_height: float = 6.4
+@export var maximum_height: float = 999
+
+func _ready() -> void:
+	var gamepad: Gamepad = get_tree().get_first_node_in_group("Gamepad")
+	gamepad.spun_clockwise.connect(_rotate_right)
+	gamepad.spun_counter_clockwise.connect(_rotate_left)
+
+func _rotate_right() -> void:
+	position.y = clampf(position.y + 1, minimum_height, maximum_height)
+
+func _rotate_left() -> void:
+	position.y = clampf(position.y - 0.05, minimum_height, maximum_height)
