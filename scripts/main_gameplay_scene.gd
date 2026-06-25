8a2b97335a3015cad38943bbd0a660bfd9953b6a
Dimitri Sukhankin <berna@detta.dev>
2026-06-25 16:08:40 +0400

    added basic logic for switching minigames

--- scripts/main_gameplay_scene.gd
+++ scripts/main_gameplay_scene.gd
@@ -3,9 +3,9 @@ extends Node3D
 @export var sub_viewport: SubViewport
 
 @export var title_screen_scene: PackedScene
-@export var minigame_0_scene: PackedScene
-@export var minigame_1_scene: PackedScene
-@export var minigame_2_scene: PackedScene
+@export var minigame_scenes: Array[PackedScene]
+var current_minigame_index: int = 0
+var current_minigame_in_scene: Node
 
 @export var fade_in: ColorRect
 
@@ -30,8 +30,9 @@ func _ready() -> void:
 	await fake_leader.say_line("res://sound/leader-bet-youve-never.wav", "Haha, bet you've never seen\nanything like this before, mate.")
 	await get_tree().create_timer(2.0).timeout
 
-	var title_screen = title_screen_scene.instantiate()
-	sub_viewport.add_child(title_screen)
+	no_signal_card.hide()
+	current_minigame_in_scene = title_screen_scene.instantiate()
+	sub_viewport.add_child(current_minigame_in_scene)
 
 	await get_tree().create_timer(12.5).timeout
 
@@ -40,3 +41,17 @@ func _ready() -> void:
 
 	await get_tree().create_timer(3.0).timeout
 	await fake_leader.say_line("res://sound/leader-minigame-explanation.wav", "Okay, all minigames are simple:\njust spin the joystick, and\noccasionally press A.\nLet's go.")
+	await current_minigame_in_scene.fade_in()
+	_load_current_minigame()
+
+func _load_current_minigame() -> void:
+	if current_minigame_in_scene:
+		current_minigame_in_scene.queue_free()
+		await get_tree().process_frame
+	current_minigame_in_scene = minigame_scenes[current_minigame_index].instantiate()
+	sub_viewport.add_child(current_minigame_in_scene)
+	current_minigame_in_scene.restart.connect(_load_current_minigame)
+	current_minigame_in_scene.next_minigame.connect(_next_minigame)
+
+func _next_minigame() -> void:
+	print("Next minigame requested but I'm lazy")
--- scripts/party/party_title_screen.gd
+++ scripts/party/party_title_screen.gd
@@ -1,5 +1,11 @@
 extends Control
 
+@export var white_screen: ColorRect
+
 func _ready() -> void:
     $AnimationPlayer.play("title_screen_animation")
- 
+
+func fade_in() -> void:
+    var tween := create_tween()
+    tween.tween_property(white_screen, "color:a", 1.0, 1.0)
+    await tween.finished
